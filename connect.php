7fa585b53c7d58ae19a1e9129adc6632249feaa6
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-06-21 15:06:06 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@
     //database connection
     $con = new mysqli('localhost', 'root', '', 'mayflower');
     if($con->connect_error){
-        die('Connection Failed :'.$con->connect_error);
+        die('Connection Failed :' .$con->connect_error);
     }else{
         $stmt = $con->prepare("insert into registration(Name, Email) values(?, ?)");
         $stmt->bind_param("ss",$Name, $Email);
